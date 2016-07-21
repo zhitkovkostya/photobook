a2760ebea6b643ed96cc1e117171370047fdc996
dorvic <dorofeevvictorv@gmail.com>
2016-07-21 21:56:06 +0300

    JS (components): add User component

---
+++ source/js/app.js
@@ -0,0 +1,5 @@
+var User = require('./components/user/user.js');
+if (document.querySelector('.user-panel')) {
+    new User('.user-panel');
+}
+
---
+++ source/js/components/user/user.js
@@ -0,0 +1,59 @@
+
+var VK = require('../apiVK/index.js');
+var compileFn = require("jade!./template.pug");
+
+/**
+ * Пользователь вконтакте
+ * @class
+ */
+
+class User {
+
+    constructor(selector) {
+        if (typeof selector !== 'string') {
+           throw new Error('Selector must be a string.');
+        }
+
+        this.rootEl = document.querySelector(selector);
+
+        if (!this.rootEl) {
+            throw new Error(`Element not found: ${selector}`);
+        }
+
+        this._loadUser()
+            .then(response => this._readUser(response[0]))
+            .then(() => this._renderUser());
+    }
+
+    /**
+     * Метод для получения информации о пользователе
+     * @returns {Promise} - Данные ответа сервера
+     */
+    _loadUser() {
+        return VK.callApi('users.get', { fields: 'photo_200, status' });
+    }
+
+    /**
+     * Метод для записи информации в класс User
+     * @param {Object} response - Данные ответа сервера
+     */
+    _readUser(response) {
+        this.name = `${response.first_name} ${response.last_name}`;
+        this.url = response.photo_200;
+        this.status = response.status;
+    }
+
+    /**
+     * Метод для добавлении информации о пользователе на страницу
+     */
+    _renderUser() {
+        this.rootEl.innerHTML = compileFn({
+            name: this.name,
+            photo: this.url,
+            status: this.status
+        });
+
+    }
+}
+
+module.exports = User;
